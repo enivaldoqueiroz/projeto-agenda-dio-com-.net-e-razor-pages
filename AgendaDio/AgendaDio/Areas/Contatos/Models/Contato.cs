88afe8c3311230676167681a04db225b356830a9
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-12-16 15:29:21 -0300

    Ajustes

--- AgendaDio/AgendaDio/Areas/Contatos/Models/Contato.cs
+++ AgendaDio/AgendaDio/Areas/Contatos/Models/Contato.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace AgendaDio.Areas.Contatos.Models
 {
     public class Contato
     {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Id { get; set; }
+        public string Id { get; set; }
+        public DateTime Id { get; set; }
+        public string Id { get; set; }
+        [NotMapped]
+        public IFormFile Foto { get; set; }
     }
 }
