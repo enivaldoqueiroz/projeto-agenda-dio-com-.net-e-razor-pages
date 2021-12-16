e3b825a7cb99b8751eccd4b7ca56450dab6f739e
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-12-16 15:27:45 -0300

    Implementando a classe Contato e as Paginas

---
+++ AgendaDio/AgendaDio/Areas/Contatos/Models/Contato.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendaDio.Areas.Contatos.Models
+{
+    public class Contato
+    {
+    }
+}
