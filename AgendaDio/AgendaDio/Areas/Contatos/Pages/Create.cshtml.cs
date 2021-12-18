ed3242745251531cd9ca99d67c0b2e2ae3f83aa6
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-12-18 19:28:44 -0300

    Implementação do medoto para salvar imagem e da interface IGenericRepository

--- AgendaDio/AgendaDio/Areas/Contatos/Pages/Create.cshtml.cs
+++ AgendaDio/AgendaDio/Areas/Contatos/Pages/Create.cshtml.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using AgendaDio.Areas.Contatos.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,19 +13,32 @@ namespace AgendaDio.Areas.Contatos.Pages
 {
     public class CreateModel : PageModel
     {
+        private IWebHostEnvironment _env;
+
         [BindProperty]
         public Contato Contato { get; set; }
+
+        public CreateModel(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
         public void OnGet()
         {
         }
-        public async IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync(CancellationToken cancelationToken)
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
             //salvar a image
-            Contato.Foto.CopyToAsync();
+            Contato.Id = Guid.NewGuid();
+            Contato.FotoUrl = Path.Combine("Images", "Cotatos", $"{Contato.Id} - {Contato.Foto.FileName}");
+            var fulPath = Path.Combine(_env.WebRootPath, Contato.FotoUrl);
+            using (var fileStream = new FileStream(Contato.FotoUrl, FileMode.Create))
+            {
+                await Contato.Foto.CopyToAsync(fileStream, cancelationToken);
+            }
 
             //todo: persistir os dados
             return RedirectToPage("Index");
