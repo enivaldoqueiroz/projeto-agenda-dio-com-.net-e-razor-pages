c8560d3975d019659d1b53e303dfc2cf24902a98
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-12-18 20:23:15 -0300

    Implementação da classe GenericRepository

--- AgendaDio/AgendaDio/Shared/Data/AgendaDbContext.cs
+++ AgendaDio/AgendaDio/Shared/Data/AgendaDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace AgendaDio.Shared.Data
+﻿using AgendaDio.Areas.Contatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaDio.Shared.Data
 {
-    public class AgendaDbContext
+    public class AgendaDbContext : DbContext
     {
+        public AgendaDbContext(DbContextOptions<AgendaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+
     }
 }
